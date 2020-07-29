24143d2e565d8e9aa5935080e2d61fd03e91c727
vixtor-qm <vctrshr@gmail.com>
2020-07-29 18:30:34 +0300

    Update kvsWebRTCClientMasterGstreamerSample.c
    added config for rpi support with the hardware encoder omxh264enc

--- samples/kvsWebRTCClientMasterGstreamerSample.c
+++ samples/kvsWebRTCClientMasterGstreamerSample.c
@@ -155,9 +155,25 @@ PVOID sendGstreamerAudioVideo(PVOID args)
                     &error);
             } else {
                 pipeline = gst_parse_launch(
-                    "autovideosrc ! queue ! videoconvert ! video/x-raw,width=1280,height=720,framerate=[30/1,10000000/333333] ! "
-                    "x264enc bframes=0 speed-preset=veryfast bitrate=512 byte-stream=TRUE tune=zerolatency ! "
-                    "video/x-h264,stream-format=byte-stream,alignment=au,profile=baseline ! appsink sync=TRUE emit-signals=TRUE name=appsink-video",
+                    
+                    //"autovideosrc ! queue ! videoconvert ! video/x-raw,width=1280,height=720,framerate=[30/1,10000000/333333] ! "
+                    //"x264enc bframes=0 speed-preset=veryfast bitrate=512 byte-stream=TRUE tune=zerolatency ! "
+                    //config for the raspberry pi with omxh264enc
+                    
+                    //"video/x-h264,stream-format=byte-stream,alignment=au,profile=baseline ! appsink sync=TRUE emit-signals=TRUE name=appsink-video",
+                    
+                     //"v4l2src do-timestamp=TRUE device=/dev/video0 ! videoconvert ! "
+                    "autovideosrc ! "
+                    //"queue ! "
+                    "videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! "
+                    "omxh264enc control-rate=1 target-bitrate=1000000 periodicity-idr=45 inline-header=false ! "
+                     "h264parse config-interval=-1 ! "
+                   //"x264enc bframes=0 speed-preset=veryfast bitrate=512 byte-stream=TRUE tune=zerolatency ! "
+                   "video/x-h264,stream-format=byte-stream,alignment=au,profile=baseline ! "
+                   //"video/x-h264,stream-format=byte-stream,alignment=au,width=640,height=480,framerate=30/1 profile=baseline ! "
+                   "appsink sync=TRUE emit-signals=TRUE name=appsink-video",
+
+                    
                     &error);
             }
             break;
